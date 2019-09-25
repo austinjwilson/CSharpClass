76d0248ffc55c0d50b088bb52fbc9014f7d32381
Austin Wilson <austinjwilson1989@gmail.com>
2019-09-25 09:16:17 -0500

    Lesson 6 - Strings and more

--- Program.cs
+++ Program.cs
@@ -10,8 +10,26 @@ namespace CSharpClass
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
-            MyLesson5Examples();
+            //MyLesson5Examples();
+            MyLesson6Examples();
         } // End Method Main
+
+        static void MyLesson6Examples()
+        {
+            Lesson6 mySix = new Lesson6();
+            mySix.MyTrimExample();
+            mySix.MyEqualsExample();
+            mySix.MyLowerUpperExample();
+            mySix.MyStringLength();
+            string myJoined = mySix.MyJoinedStrings("Happy","Coding");
+            Console.WriteLine(myJoined);
+            string myFav = mySix.MyStringBuilder("pizza", 3);
+            Console.WriteLine(myFav);
+            mySix.MyExampleChar();
+            mySix.MyEscapeExample();
+            mySix.MyPlaceHolderExample("mario brothers", "classic");
+
+        } // end method my lesson 6 examples
 
         static void MyLesson5Examples()
         {
@@ -20,5 +38,5 @@ namespace CSharpClass
             myFive.LongFromInt(); 
         } // end method my lesson 5 examples  
 
-    } // End Class
+    } // End class
 } // end namspace 
